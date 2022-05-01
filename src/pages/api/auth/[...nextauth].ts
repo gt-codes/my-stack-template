5bde941d5df57e728d786ea3ea08fb2dc13f1a10
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-04-30 22:04:03 -0500

    refactor: updated next auth config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,15 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+	session: {
+		strategy: 'jwt',
+		maxAge: 30 * 24 * 60 * 60, // 30 days
+	},
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
@@ -9,4 +17,6 @@ export default NextAuth({
 		}),
 	],
 	secret: process.env.NEXTAUTH_SECRET,
+	// Uncomment this line if connecting to a real database
+	// adapter: PrismaAdapter(prisma),
 });
